450b66dde1526280630467cbfc381d722e4dbbf2
Nerond32 <nerond32@gmail.com>
2019-05-22 00:13:00 +0200

    Header stylized closely to the mock one

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const HeaderWrapper = styled.header`
+  align-items: center;
+  justify-items: center;
+  border-bottom: 1px solid white;
+  height: 3.5em;
+  margin: 0 1em 0.5em 1em;
+  display: flex;
+  img {
+    flex: 1;
+    width: 1.3em;
+    height: 1.3em;
+    order: 5;
+  }
+  .header-exit-button {
+    justify-self: flex-end;
+    order: 10;
+  }
+`;
 
 const Header = () => {
   return (
-    <header>
+    <HeaderWrapper>
       <img src="./awaymoFullWhite.svg" alt="Awaymo  avatar" />
-      <FontAwesomeIcon icon="times" />
-    </header>
+      <FontAwesomeIcon className="header-exit-button" icon="times" />
+    </HeaderWrapper>
   );
 };
 
